898e1f59b9cc23d992ff07d187fce130542e19ff
Fergus Duncan <fergus_duncan@bitwisegroup.com>
2017-11-10 15:28:45 +0000

    initial commit

---
+++ tests/test_ping.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+
+import os
+
+def performPing(hostname):
+    response = os.system("ping -c 1 " + hostname)
+    if(response==0):
+        return 1
+    else:
+        return 0
+
+def test_checkPing_google_com():
+    assert(performPing("google.com") == 1)
+
+def test_checkPing_xgoogle_com():
+    assert(performPing("xgoogle.com") == 0)
+
