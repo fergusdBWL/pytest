031444f2e41176aaa864ed90c21d24d9af2e3035
Fergus Duncan <fergus_duncan@bitwisegroup.com>
2017-11-13 14:56:52 +0000

    update

--- test_set_1/test_ping.py
+++ test_set_1/test_ping.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python
 
 import os
+from utils import pinger
 
-def performPing(hostname):
-    response = os.system("ping -c 1 " + hostname)
-    if(response==0):
-        return 1
-    else:
-        return 0
+#def performPing(hostname):
+#    response = os.system("ping -c 1 " + hostname)
+#    if(response==0):
+#        return 1
+#    else:
+#        return 0
 
 def test_checkPing_google_com():
     assert(performPing("google.com") == 1)
