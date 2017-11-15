218d44dfeae3ba124ba48955464f361bddde36a1
John Cameron <j_cameron@bitwisegroup.com>
2017-11-15 14:25:49 +0000

    Made a test fail for demo

--- test_set_1/test_ping.py
+++ test_set_1/test_ping.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python
+import sys, os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, myPath + '/../utils/')
+
 # do some network pinging using the PingUtils package
 # this package is part of the test infrastructure defined here
 
@@ -10,7 +14,7 @@ def test_checkPing_google_com():
     assert(pingUtils.singlePing("google.com") == 1)
 
 def test_checkPing_xgoogle_com():
-    assert(pingUtils.singlePing("xgoogle.com") == 0)
+    assert(pingUtils.singlePing("xgoogle.com") == 1)
 
 def test_checkPing_bbc_com():
     assert(pingUtils.singlePing("bbc.com") == 1)
