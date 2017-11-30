d1274bf71998234308c7337dfb122009dc264274
John Cameron <33278654+johncmn@users.noreply.github.com>
2017-11-30 16:16:08 +0000

    Tidied up some pyton. Added some error checking.

--- utils/Create_results.py
+++ utils/Create_results.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
-# This file finds all the results.xml files following a test run,
-# combines them into a single xml file, and transforms this xml file
-# into html using an xslt transform. The resulting html file is then 
-# converted to pdf.
+"""
+ This file finds all the results.xml files following a test run,
+ combines them into a single xml file, and transforms this xml file
+ into html using an xslt transform. The resulting html file is then 
+ converted to pdf.
+"""
 
 import os, os.path
-import glob
 import xmlcombine
 import subprocess
 import lxml.etree as ET
@@ -17,31 +18,75 @@ html_file = 'combined_results.html'
 pdf_file = 'Test_Results.pdf'
 
 
-# Find all the results files.
-results_files = []
-for root, dirs, files in os.walk('.'):
-  for f in files:
-    fullpath = os.path.join(root, f)
-    if f.endswith(source_files):
-      results_files.append(fullpath)
+### check all the files exist
+def FileExists(fileName):
+  if os.path.isfile(fileName):
+    return True
+  else:
+    return False
 
-# combine the files
-with open (combined_file, 'w') as f:
-  # write header, with link to xslt file
-  f.write ('<?xml version="1.0" encoding="UTF-8"?> \
-       <?xml-stylesheet type="text/xsl" href="results.xsl"?>')
-  # write actual file
-  f.write(xmlcombine.combine (results_files))
 
-# convert to html.
-dom = ET.parse(combined_file)
-xslt = ET.parse(transform_file)
-# do the transform
-transform = ET.XSLT(xslt)
-newdom = transform(dom)
-# write out to html file
-with open (html_file, 'w') as f:
-  f.write (ET.tostring(newdom, pretty_print=True))
+### Find all the results files.
+def GetResultsFiles():
+  print "Find all results files"
+  results_files = []
 
-# convert to pdf
-subprocess.call (['/usr/local/bin/wkhtmltopdf', html_file, pdf_file]) 
+  for root, dirs, files in os.walk('.'):
+    for f in files:
+      fullpath = os.path.join(root, f)
+      if f.endswith(source_files):
+        results_files.append(fullpath)
+  print "found " + str(len(results_files)) + " results files"
+  if len(results_files) == 0:
+    print "Error: No results files found! Exiting"
+    exit(-1)
+  return results_files
+
+
+### combine the files
+def CombineResultsFiles(results_files):
+  print "Combine the results files"
+  with open (combined_file, 'w') as f:
+    # write header, with link to xslt file
+    f.write ('<?xml version="1.0" encoding="UTF-8"?> \
+         <?xml-stylesheet type="text/xsl" href="results.xsl"?>')
+    # write actual file
+    f.write(xmlcombine.combine (results_files))
+  if not FileExists(combined_file):
+    print "Error: Failed to create combined xml file."
+    exit(-1)
+
+### convert to html.
+def ConvertToHtml():
+  print "Transform xml to html"
+  if not FileExists(transform_file):
+    print "Transform file not found!"
+    exit(-1)
+
+  dom = ET.parse(combined_file)
+  xslt = ET.parse(transform_file)
+  # do the transform
+  transform = ET.XSLT(xslt)
+  newdom = transform(dom)
+  # write out to html file
+  with open (html_file, 'w') as f:
+    f.write (ET.tostring(newdom, pretty_print=True))
+
+  if not FileExists(html_file):
+    print "Failed to created html file"
+    exit(-1)
+
+### convert to pdf
+def ConvertToPdf():
+  print "convert to pdf for publishing"
+  subprocess.call (['/usr/local/bin/wkhtmltopdf', html_file, pdf_file]) 
+  if not FileExists(pdf_file):
+    print "Failed to Create pdf file"
+    exit(-1)
+
+######
+CombineResultsFiles(GetResultsFiles())
+ConvertToHtml()
+ConvertToPdf()
+exit(0)
+
--- utils/xmlcombine.py
+++ utils/xmlcombine.py
@@ -10,17 +10,22 @@ def combine(files):
     total_errors = 0
     total_failures = 0
 
+    # loop through input files
     for filename in files:
         testset = str(os.path.abspath(filename))
         testset = testset.encode("string_escape").rsplit('/', 2)[1]
-        e = ElementTree.Element("testset")
+        # the incoming xml
         data = ElementTree.parse(filename).getroot()
+        # create a new element for each testset
+        e = ElementTree.Element("testset")
 
+        # get test result data from the input file
         skips = int(data.attrib.get('skips'))
         tests = int(data.attrib.get('tests'))
         errors = int(data.attrib.get('errors'))
         failures = int(data.attrib.get('failures'))
 
+        # set the value in the new "testset" element
         e.set('tests', str(tests))
         e.set('skips', str(skips))
         e.set('errors', str(errors))
@@ -28,25 +33,34 @@ def combine(files):
         e.set('passes', str(tests - skips - failures))
         e.set('name', testset)
 
+        # collate totals
         total_skips = total_skips + skips
         total_tests = total_tests + tests
         total_errors = total_errors + errors
         total_failures = total_failures + failures
 
+        # if the file has a properties element append it to new element, 
+        # then remove it from existing element
         for properties in data.findall('properties'):
             e.append(properties)
             data.remove(properties)
 
+        # if the file has a testcase element append it to new element, 
+        # then remove it from existing element
         for testcase in data.findall('testcase'):
             e.append(testcase)
             data.remove(testcase)
 
+        # add our new element to the output.
         data.append(e)
 
+        # if the first element hasn't been written to yet, add our data element to it.
+        # if it has, append our latest data element
         if first is None:
             first = data
         else:
             first.extend(data)
+    # append the totals to the file if it's not empty
     if first is not None:
         first.set('tests', str(total_tests))
         first.set('skips', str(total_skips))
